ef3b209226901ba2ebe243ad3c45204164a4e788
eliana-bb <63822830+eliana-bb@users.noreply.github.com>
2024-02-06 15:08:13 -0600

    create a function to parse various number formats

---
+++ number_reader.py
@@ -0,0 +1,18 @@
+import re
+
+pattern_decimal = re.compile(pattern=r"^(?:0d)?(\d+)$", flags=re.IGNORECASE)
+pattern_binary = re.compile(pattern=r"^(?:%|0b)((?:[01]{8}){1,2})$", flags=re.IGNORECASE)
+pattern_hexadecimal = re.compile(pattern=r"^(?:0x|\$)((?:[0-9a-f]{2}){1,2})$", flags=re.IGNORECASE)
+
+def read(test_str: str) -> int:
+	test_str = test_str.replace("_", "")
+	dec_match = pattern_decimal.fullmatch(test_str)
+	if dec_match is not None:
+		return int(dec_match.group(1), 10)
+	hex_match = pattern_hexadecimal.fullmatch(test_str)
+	if hex_match is not None:
+		return int(hex_match.group(1), 16)
+	bin_match = pattern_binary.fullmatch(test_str)
+	if bin_match is not None:
+		return int(bin_match.group(1), 2)
+	raise ValueError(f"Could not parse number {test_str}")
