01cc201a3073a5d2e1228ca5f7cf1c7d4b10bad6
eliana-bb <63822830+eliana-bb@users.noreply.github.com>
2024-02-07 10:26:02 -0600

    implemented preprocessor

--- iron_assembler.py
+++ iron_assembler.py
@@ -1,8 +1,27 @@
+import re
+
+
 class Assembler:
 	def __init__(self, file_path: str):
 		self.main_asm_file = file_path
+		self.text_lines: list[str] = []
 
 	def assemble(self):
+
+		self.preprocess_lines()
 		print("Called assemble!")
-		if self:
-			pass
+		print(self.text_lines)
+
+	def preprocess_lines(self) -> None:
+		with open(self.main_asm_file) as infile:
+			file_content = infile.read()
+		file_content = file_content.replace(":", ":\n")
+		out_lines = []
+		for line in file_content.split("\n"):
+			re_match = re.fullmatch(r"(.*?);.*|(.+)", line)
+			if re_match is None:
+				continue
+			processed_line = re_match.group(1) or re_match.group(2)  # One of these is always None
+			if processed_line not in ("", None):
+				out_lines.append(processed_line)
+		self.text_lines = out_lines
