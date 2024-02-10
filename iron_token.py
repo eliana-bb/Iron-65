645342131775b96c0ab038dc458b2ca9ed6f59b1
eliana-bb <63822830+eliana-bb@users.noreply.github.com>
2024-02-10 15:41:37 -0600

    nuke a bunch of functionality to move to a different library

--- iron_token.py
+++ iron_token.py
@@ -2,58 +2,21 @@ from number_reader import read
 
 
 class Tokenizer:
-	def __init__(self):
-		self.prog_tokens: list[Token] = []
-		self.symbols: dict[str, Symbol] = {}
-		self.labels: dict[str, Label] = {}
-		self.anon_labels: list[Label] = []
-		self.cart_config: list[Token] = []
-		self.cursor = 0
-
-	def tokenize(self, text_lines: list[str]):
+	def __init__(self, text_lines: list[str]):
+		self.tokens: list[Token] = []
 		for line in text_lines:
-			line = line.strip().upper()
-			this_token = Token(line)
-			if this_token.token_type == "CART_CONFIG":
-				self.cart_config.append(this_token)
-			else:
-				self.prog_tokens.append(this_token)
-
+			self.tokens.append(Token(line))
 
 class Token:
 	def __init__(self, line: str):
-		if line.startswith("!"):
-			self.token_type = "CART_CONFIG"
-		elif line.startswith("."):
-			self.token_type = "DATA_RAW"
-		elif line.endswith(":"):
-			self.token_type = "LABEL_DEF"
-		elif "=" in line:
-			self.token_type = "SYMBOL_DEF"
-		else:
-			self.token_type = "OPCODE"
 		self.content = line
-
-
-class Symbol:
-	def __init__(self, declaration: str):
-		self.value = read(declaration)
-
-	def is_byte_sized(self) -> bool:
-		return self.value <= 0xFF
-
-	def get_byte(self) -> bytes:
-		return self.value.to_bytes(1)
-
-	def get_word(self) -> bytes:
-		return self.value.to_bytes(2, "little")
-
-	def get_high_byte(self) -> bytes:
-		return (self.value >> 8).to_bytes(1)
-
-	def get_low_byte(self) -> bytes:
-		return (self.value & 0xFF).to_bytes(1)
-
-
-class Label:
-	pass
+		if line[0] == "!":
+			self.type = "CART_CONFIG"
+		elif line[0] == ".":
+			self.type = "RAW_DATA"
+		elif line[-1] == ":":
+			self.type = "LABEL"
+		elif "=" in line:
+			self.type = "SYMBOL"
+		else:
+			self.type = "OPCODE"
