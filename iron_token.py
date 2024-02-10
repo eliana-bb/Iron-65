acf531207a3fe307b18366d38ddb01a9cf396e1b
eliana-bb <63822830+eliana-bb@users.noreply.github.com>
2024-02-10 12:53:53 -0600

    implement basic tokenizer

--- iron_token.py
+++ iron_token.py
@@ -3,14 +3,37 @@ from number_reader import read
 
 class Tokenizer:
 	def __init__(self):
-		self.tokens: list[Token] = []
+		self.prog_tokens: list[Token] = []
 		self.symbols: dict[str, Symbol] = {}
 		self.labels: dict[str, Label] = {}
 		self.anon_labels: list[Label] = []
+		self.cart_config: list[Token] = []
+		self.cursor = 0
+
+	def tokenize(self, text_lines: list[str]):
+		for line in text_lines:
+			line = line.strip().upper()
+			this_token = Token(line)
+			if this_token.token_type == "CART_CONFIG":
+				self.cart_config.append(this_token)
+			else:
+				self.prog_tokens.append(this_token)
 
 
 class Token:
-	pass
+	def __init__(self, line: str):
+		if line.startswith("!"):
+			self.token_type = "CART_CONFIG"
+		elif line.startswith("."):
+			self.token_type = "DATA_RAW"
+		elif line.endswith(":"):
+			self.token_type = "LABEL_DEF"
+		elif "=" in line:
+			self.token_type = "SYMBOL_DEF"
+		else:
+			self.token_type = "OPCODE"
+		self.content = line
+
 
 class Symbol:
 	def __init__(self, declaration: str):
@@ -30,6 +53,7 @@ class Symbol:
 
 	def get_low_byte(self) -> bytes:
 		return (self.value & 0xFF).to_bytes(1)
+
 
 class Label:
 	pass
